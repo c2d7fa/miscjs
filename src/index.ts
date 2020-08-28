a8cac0a23237adcdf39a45bd49bea5eca13f1689
Jonas Hvid <mail@johv.dk>
2020-08-28 22:54:19 +0200

    Add 'implies' operator

--- src/index.ts
+++ src/index.ts
@@ -89,3 +89,9 @@ export function unwords(words: string[]): string {
   }
   return result;
 }
+
+// Returns `true` of `b` implies `c` (in the sense of a material conditional),
+// `false` otherwise.
+function implies(b: boolean, c: boolean) {
+  return !b || (b && c);
+}
